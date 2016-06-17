055803219c4ade72db875f193e4c8ac4e7c06d2f
e-miya <miyazaki_e@miyazakieimi-no-macbook-air.local>
2016-06-17 10:21:42 +0900

    新規

---
+++ SlideshowApp/ResultViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultViewController.swift
+//  SlideshowApp
+//
+//  Created by 宮崎英美 on 2016/06/16.
+//  Copyright © 2016年 taro.kirameki. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var resultimage: UIImageView!
+    var imangeName:String = "NoImage"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        resultimage.image = UIImage(named: imangeName)!
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func show(sender:UIImageView){
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,16 +10,103 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var playstopButton: UIButton!
+    @IBOutlet weak var nextImage: UIButton!
+    @IBOutlet weak var backImage: UIButton!
+    var imageName:String = "cat"
+    var number:Int = 1
+    var timer:NSTimer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //デフォルトの表示画像を設定
+        let image = UIImage(named: "\(imageName)" + number.description + ".jpg")!
+        imageView.image = image
+        
+        
+        
+        //ImageViewのタップイベントを設定
+        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTap(_:)))
+        self.imageView.addGestureRecognizer(tapGesture)
+        
+        //各ボタンのタップイベントを設定
+        playstopButton.addTarget(self, action: #selector(ViewController.buttonTap), forControlEvents: .TouchUpInside)
+        nextImage.addTarget(self, action: #selector(ViewController.buttonTap), forControlEvents: .TouchUpInside)
+        backImage.addTarget(self, action: #selector(ViewController.buttonTap), forControlEvents: .TouchUpInside)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //timerの設定
+    func timerInit() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: #selector(ViewController.imageChange as (ViewController) -> () -> ()), userInfo: nil, repeats: true)
+    }
+    //timerの開始
+    func timerStart(sender : UIImageView){
+        if(!timer.valid){
+            timerInit()
+        }
+    }
+    //timerの停止
+    func timerStop(){
+        timer?.invalidate()
+    }
+    //ボタンタップ時の動作
+    func buttonTap(sender:UIButton){
+        switch sender.tag {
+        case 1:
+            if sender.titleLabel?.text == "再生" {
+                timerInit()
+                sender.setTitle("停止", forState: UIControlState.Normal)
+            }else if sender.titleLabel?.text == "停止" {
+                timerStop()
+                sender.setTitle("再生", forState: UIControlState.Normal)
+            }
+        case 2:
+            imageChange(1)
+        case 3:
+            imageChange(-1)
+        default:
+            break
+        }
+    }
+    //画像切り替え（timerからの呼び出し）
+    func imageChange(){
+        imageChange(1)
+    }
+    //画像切り替え
+    func imageChange(count:Int){
+        number = number + count
+        if number > 6 {
+            number = 1
+        }else if number < 1 {
+            number = 6
+        }
+        let image = UIImage(named: "\(imageName)" + number.description + ".jpg")!
+        imageView.image = image
+    }
+    //画像タップ時の動作
+    func imageTap(sender:UITapGestureRecognizer){
+        // ResultViewControllerへ遷移するためのSegueを呼び出す
+        performSegueWithIdentifier("toResultViewController",sender: nil)
+        
+    }
+    //segueの設定
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let resultViewContlloer:ResultViewController = segue.destinationViewController as!ResultViewController
+        resultViewContlloer.imangeName = "\(imageName)" + number.description + ".jpg"
+        
+    }
+    // 他画面からの戻り
+    @IBAction func unwind(segue:UIStoryboardSegue){
+    }
 
 }
 
